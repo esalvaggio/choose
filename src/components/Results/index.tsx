5cab54846984196cc489ab8232cf9181e30a195f
Elliot Salvaggio <elliotms98@gmail.com>
2025-03-29 19:00:59 -0700

    redirect on submit on results page

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import { ISession } from "../../interfaces/ISession";
 import styles from './index.module.scss';
 
 function Results({ session }: { session: ISession }) {
+  const navigate = useNavigate();
   const films = session.films.map((film) => ({
     filmTitle: film.title,
     count: 0,
@@ -31,7 +33,12 @@ function Results({ session }: { session: ISession }) {
         {/* <div className={styles.roundText}>end of round: {roundNumber}</div> */}
         <div className={styles.subheading}>the following has been chosen:</div>
         <div className={styles.winningTitle}>{winningFilm.filmTitle}</div>
-        <button className={styles.button}>start over</button>
+        <button 
+          className={styles.button}
+          onClick={() => navigate('/')}
+        >
+          start over
+        </button>
       </div>
     </div>
   );
